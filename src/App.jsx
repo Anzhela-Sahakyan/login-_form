66a867ce9a5f9be7408def3ef872b7fbf59786a1
Anzhela <sahakyananzhela@gmail.com>
2023-06-25 13:58:21 +0400

    delete: deleted components folder, added the code into App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,53 @@
-import FirstName from "./components/firstName/FirstName";
-import LastName from "./components/lastName/LastName";
-import Email from "./components/email/Email";
-import Password from "./components/Password/Password";
-import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = function (e) {
+    e.preventDefault();
+    console.log("First Name:", firstName);
+    console.log("Last Name:", lastName);
+    console.log("Email:", email);
+    console.log("Password:", password);
+  };
+
   return (
     <div className="App">
       <h1>Login Form</h1>
-      <FirstName />
-      <LastName />
-      <Email />
-      <Password />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="First Name"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+        <br />
+        <input
+          type="text"
+          placeholder="Last Name"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+        />
+        <br />
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <br />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <br />
+        <button type="submit">Login</button>
+      </form>
     </div>
   );
 }
